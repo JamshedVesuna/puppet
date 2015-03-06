d1e88a26d24f69d29d5d16df765d0c98076b7ae8
JamshedVesuna <jamshed.vesuna@gmail.com>
2015-03-05 22:16:07 -0800

    Update sources before installing git and puppet

--- raw-setup.sh
+++ raw-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get update
 sudo apt-get -y install git puppet
 git clone https://github.com/JamshedVesuna/puppet.git /tmp/puppet
 sudo puppet apply /tmp/puppet/jamshed.pp
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+sudo apt-get update
 sudo apt-get -y install puppet
 sudo puppet apply jamshed.pp
