14204a06c8f98443f717ce08430414b4c13d1474
JamshedVesuna <jamshed.vesuna@gmail.com>
2015-05-25 00:25:17 +0000

    Partial fix for dotfiles setup

--- jamshed.pp
+++ jamshed.pp
@@ -4,13 +4,12 @@ $paths = [ "/usr/local/bin/", "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ]
 
 user {$local_user:
      ensure => present,
-     shell  => "/bin/bash",
+     shell  => "/bin/zsh",
 }
 
 define local_package () {
   package { "$name":
     ensure  => latest,
-    #require => Exec['apt-update'],
   }
 }
 
@@ -21,10 +20,10 @@ define local_absent () {
 }
 
 local_package {
-  ## Text Editors
+  # Text Editors
   'vim':;
 
-  ## Languages
+  # Languages
   'python2.7':;
   'python3':;
   'ruby':;
@@ -32,10 +31,10 @@ local_package {
   'ruby1.9.1-dev':;
   'markdown':;
 
-  ## Providers
+  # Providers
   'python-pip':;
 
-  ## Networking
+  # Networking
   'curl':;
   'ftp':;
   'openssh-client':;
@@ -44,23 +43,23 @@ local_package {
   'wget':;
   'whois':;
 
-  ## Version Control
+  # Version Control
   'git':;
   'rdiff-backup':;
   'rsync':;
 
-  ## Cli
+  # Cli
   'zsh':;
 
-  ## Searching
+  # Searching
   'grep':;
   'ack-grep':;
   'silversearcher-ag':;
 
-  ## Processes
+  # Processes
   'htop':;
 
-  ## Misc tools
+  # Misc tools
   'bash':;
   'cron':;
   'screen':;
@@ -69,10 +68,11 @@ local_package {
   'xclip':;
 }
 
+# Remove apt-get packages
 #local_absent {
 #}
 
-### Python Pip
+## Python Pip
 
 define python_pip () {
   package { "$name":
@@ -81,6 +81,7 @@ define python_pip () {
   }
 }
 
+# Remove python-pip packages
 define remove_python_pip () {
   package { "$name":
     ensure   => absent,
@@ -95,10 +96,11 @@ python_pip {
   'urllib3':;
 }
 
+# Remove python-pip packages
 #remove_python_pip {
 #}
 
-### Ruby Gems
+## Ruby Gems
 
 define ruby_gems () {
   package { "$name":
@@ -107,6 +109,7 @@ define ruby_gems () {
   }
 }
 
+# Remove ruby gems
 define remove_ruby_gems () {
   package { "$name":
     ensure   => absent,
@@ -114,6 +117,7 @@ define remove_ruby_gems () {
   }
 }
 
+# Remove ruby gems
 ruby_gems {
   'tugboat':;
 }
@@ -121,40 +125,56 @@ ruby_gems {
 #remove_ruby_gems {
 #}
 
-### DotFiles
+## DotFiles
 
 file { [ "$local_home/scripts" ]:
   ensure => "directory",
   owner  => $local_user,
 }
 
+# True if dotfiles exists
 exec { "check_presence_dotfiles":
   command => "/bin/true",
   onlyif  => "/usr/bin/test -e $local_home/scripts/dotfiles",
 }
 
+file { "$local_home/scripts/dotfiles":
+  owner => $local_user,
+  require => Exec["download_altoduo_dotfiles"]
+}
+
 exec { "download_altoduo_dotfiles":
   require => Package['git'],
   command => "git clone https://github.com/altoduo/dotfiles.git",
-  onlyif  => "/usr/bin/test -e $local_home/scripts",
+  onlyif  => [
+    "/usr/bin/test -e $local_home/scripts",
+    "bash -c '! /usr/bin/test -e $local_home/scripts/dotfiles'",
+  ],
   cwd     => "$local_home/scripts/",
   creates => "$local_home/scripts/dotfiles",
   path    => $paths,
+  user    => $local_user,
 }
 
 # setup_altoduo_dotfiles will only install dotfiles iff not present
 exec { "setup_altoduo_dotfiles":
-  require => [ Exec["check_presence_dotfiles"], Package['git'] ],
+  require => [ Exec["check_presence_dotfiles"], Exec["download_altoduo_dotfiles"], Package['git'] ],
+  onlyif  => [
+    "/usr/bin/test -e $local_home/scripts/dotfiles",
+    "bash -c 'if ! grep -q DOTFILES_PATH $local_home/.bashrc ; then return 0 ; else return 1; fi'",
+  ],
   command => "bash -c 'source ~/.bashrc && bash scripts/setup'",
   cwd     => "$local_home/scripts/dotfiles",
   user    => $local_user,
   path    => $paths,
+  logoutput => true,
 }
 
 exec { "update_altoduo_dotfiles":
   require => [ Exec["check_presence_dotfiles"], Package['git'] ],
-  command => "git pull origin master && git submodule sync && git submodule update --init --recursive && git submodule foreach git pull origin master",
+  command => "git fetch --all && git pull origin master && git submodule sync && git submodule update --init --recursive && git submodule foreach git pull origin master",
   cwd     => "$local_home/scripts/dotfiles",
+  timeout => 1800,
   user    => $local_user,
   path    => $paths,
 }
